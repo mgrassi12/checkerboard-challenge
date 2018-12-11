1471a3fd21d679e960dd0c34ddac679107624fc8
Michael Grassi <134212@ad.feit.uts.edu.au>
2018-12-11 19:35:14 +1100

    Initial commit. Basic args into main to confirm env is working. Gitignore setup to ignore output files and IDE config files.

---
+++ src/com/checkerboardchallenge/Checkerboard.java
@@ -0,0 +1,10 @@
+package com.checkerboardchallenge;
+
+public class Checkerboard {
+
+    public static void main(String[] args) {
+        System.out.println("First argument: " + args[0]);
+        System.out.println("Second argument: " + args[1]);
+    }
+
+}
